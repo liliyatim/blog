5c6e948758929bd759023d8593d866761a526f96
Lilia <liliatimofeeva701@gmail.com>
2020-12-23 20:38:35 +0200

    update DB

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=treasure',
+    'dsn' => 'mysql:host=localhost;dbname=OdzYii2',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
